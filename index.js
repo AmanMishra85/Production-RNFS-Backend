6f9061a4381666995f4dd7a2ed54343ae216e25b
Aman Mishra <157882198+AmanMishra85@users.noreply.github.com>
2024-07-20 16:45:52 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import UserRouter from "./routes/UserRoute.js";
+import PostRouter from "./routes/PostRoute.js";
+
+// DOTENV
+dotenv.config();
+
+// MONGODB Connection
+
+// REST OBJECT
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Test ROUTES
+app.get("/", (req, res) => {
+  res
+    .status(200).send("hello sir, This is backend Testing endpoint")
+});
+
+// All ROUTES
+app.use("/api/v1/auth",UserRouter);
+app.use("/api/v1/post",PostRouter)
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// LISTEN
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running at port : ${PORT}`.bgBlue.white);
+});
